a53cb8dccc8a9a960c8472f9cc70254bfd04aa3f
Adrian Miasik <github@adrian-miasik.com>
2022-03-22 19:21:11 -0400

    Create basic input form validation for our property inspector fields
    Can no longer provide a string / null which resulted in plugin crash.

--- StreamDeckPluginsDota2/property-inspector/PluginActionPI.js
+++ StreamDeckPluginsDota2/property-inspector/PluginActionPI.js
@@ -3,3 +3,20 @@
     payload.property_inspector = 'resetCounter';
     sendPayloadToPlugin(payload);
 }
+
+// Validates the provided HTML input element (using ID) to always be a number using a regex check.
+function validateInput(inputID){
+    
+    // Grab input elements
+    const inputElement = document.getElementById(inputID);
+
+    // Create constraint
+    const regularExpression = new RegExp(/\d/g); // Numbers only
+    
+    // If our input value is not a number...
+    if(!regularExpression.test(inputElement.value))
+    {
+        // Reset value to default
+        inputElement.value = 0;
+    }
+}
