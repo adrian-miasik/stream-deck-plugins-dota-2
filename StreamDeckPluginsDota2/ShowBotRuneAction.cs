90360d73c6b53025b2033e6df6e436ef2dc0507d
Adrian Miasik <git@adrian-miasik.com>
2022-04-08 14:02:19 -0400

    Create RuneBase to avoid redundancy

--- StreamDeckPluginsDota2/ShowBotRuneAction.cs
+++ StreamDeckPluginsDota2/ShowBotRuneAction.cs
@@ -1,42 +1,23 @@
 ﻿using BarRaider.SdTools;
-using WindowsInput;
 
 namespace StreamDeckPluginsDota2
 {
     [PluginActionId("com.adrian-miasik.sdpdota2.show-bot-rune")]
-    public class ShowBotRuneAction : PluginBase
+    public class ShowBotRuneAction : RuneBase
     {
-        private InputSimulator m_inputSimulator;
-        
-        public ShowBotRuneAction(ISDConnection connection, InitialPayload payload) : base(connection, payload)
+        protected ShowBotRuneAction(ISDConnection connection, InitialPayload payload) : base(connection, payload)
         {
-            m_inputSimulator = new InputSimulator();
         }
 
         public override void KeyPressed(KeyPayload payload)
         {
             // TODO: Pull string from cfg file
-            m_inputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.F14);
+            InputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.F14);
         }
 
         public override void KeyReleased(KeyPayload payload)
         {
-        }
-
-        public override void ReceivedSettings(ReceivedSettingsPayload payload)
-        {
-        }
-
-        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload)
-        {
-        }
-
-        public override void OnTick()
-        {
-        }
-
-        public override void Dispose()
-        {
+            InputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.F1);
         }
     }
 }
--- StreamDeckPluginsDota2/ShowTopRuneAction.cs
+++ StreamDeckPluginsDota2/ShowTopRuneAction.cs
@@ -1,42 +1,23 @@
 ﻿using BarRaider.SdTools;
-using WindowsInput;
 
 namespace StreamDeckPluginsDota2
 {
     [PluginActionId("com.adrian-miasik.sdpdota2.show-top-rune")]
-    public class ShowTopRuneAction : PluginBase
+    public class ShowTopRuneAction : RuneBase
     {
-        private InputSimulator m_inputSimulator;
-        
-        public ShowTopRuneAction(ISDConnection connection, InitialPayload payload) : base(connection, payload)
+        protected ShowTopRuneAction(ISDConnection connection, InitialPayload payload) : base(connection, payload)
         {
-            m_inputSimulator = new InputSimulator();
         }
 
         public override void KeyPressed(KeyPayload payload)
         {
             // TODO: Pull string from cfg file
-            m_inputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.F13);
+            InputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.F13);
         }
 
         public override void KeyReleased(KeyPayload payload)
         {
-        }
-
-        public override void ReceivedSettings(ReceivedSettingsPayload payload)
-        {
-        }
-
-        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload)
-        {
-        }
-
-        public override void OnTick()
-        {
-        }
-
-        public override void Dispose()
-        {
+            InputSimulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.F1);
         }
     }
 }
